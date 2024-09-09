718354968184516d4f6b69131fbf0cadeca5fb48
Jack Lu <jacklu0831@gmail.com>
2024-09-09 18:07:27 -0400

    working training dpo diffusion

--- sd15.sh
+++ sd15.sh
@@ -1,24 +1,19 @@
-export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
-export VAE="madebyollin/sdxl-vae-fp16-fix"
+export MODEL_NAME="pt-sk/stable-diffusion-1.5"
 export DATASET_NAME="yuvalkirstain/pickapic_v2"
 
-
 # Effective BS will be (N_GPU * train_batch_size * gradient_accumulation_steps)
-# Paper used 2048. Training takes ~30 hours / 200 steps
+# Paper used 2048. Training takes ~24 hours / 2000 steps
 
 accelerate launch train.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
-  --pretrained_vae_model_name_or_path=$VAE \
   --dataset_name=$DATASET_NAME \
   --train_batch_size=1 \
   --dataloader_num_workers=16 \
   --gradient_accumulation_steps=128 \
   --max_train_steps=2000 \
-  --lr_scheduler="constant_with_warmup" --lr_warmup_steps=200 \
+  --lr_scheduler="constant_with_warmup" --lr_warmup_steps=500 \
   --learning_rate=1e-8 --scale_lr \
-  --cache_dir="/export/share/datasets/vision_language/pick_a_pic_v2/" \
-  --checkpointing_steps 200 \
+  --cache_dir="./pick_a_pic_v2/" \
+  --checkpointing_steps 500 \
   --beta_dpo 5000 \
-   --sdxl  \
-  --output_dir="tmp-sdxl"
-  
+   --output_dir="output"
